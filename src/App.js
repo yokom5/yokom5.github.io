f567cecf34b3c6eb7b09e3a6a65040c80d228504
Yoko Nishida <53156870+yokotatenaname@users.noreply.github.com>
2020-08-02 23:04:10 +0900

    update

--- src/App.js
+++ src/App.js
@@ -9,22 +9,12 @@ function App() {
       <header className="App-header">
         <div className="Button-flex">
           <a href="https://github.com/yokotatenaname"><Button name='GitHub' /></a>
-          <a href="https://facebook.com"><Button name='Facebook' /></a>
-          <a href="https://note.com/redorangex"><Button name='Blog' /></a>
           <a href='https://www.lancers.jp/profile/nishi_001?ref=side_menu'><Button name='Lancers' /></a>
           <a href='https://crowdworks.jp/public/employees/1496186?ref=mypage_nav1_account'><Button name='CrowdWorks' /></a>
         </div>
       </header>
       <Profile>
       </Profile>
-      <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
     </div>
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -3,17 +3,21 @@ import './Profile.css';
 
 export default class Profile extends Component {
     render() {
-        // You can use them as regular CSS styles
         return (
             <div className="Profile">
                 <div className="Profile-box">
                     <div className="Profile-title">Summary</div>
-                    - system engineer of business system
+                    - hogehoge
                     <br/>
-                    <div className="Profile-title">Work Experience</div>
-                    - 2017-2020, Develop financial service system for oversea.
+                    <div className="Profile-title">Skill</div>
+                    - hogehoge
+                    <br />
+                    <div className="Profile-title">Work History</div>
+                    - 3years, Engineer of financial service system.
                     <br/>
-                    - 2020, Develop enterprise system of Japan company.
+                    - Engineer of enterprise system.
+                    <div className="Profile-title">Activity</div>
+                    - hogehoge
                 </div>
             </div>
         );
